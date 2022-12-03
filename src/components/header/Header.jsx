edb1cc9d8b6c3dcb2c3527ebb175e974665964c0
zubair2001 <zubair_mansuri2001@outlook.com>
2022-12-04 00:17:39 +0500

    DAY 3

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,7 +7,7 @@ import 'react-date-range/dist/styles.css'; // main css file
 import 'react-date-range/dist/theme/default.css'; // theme css file
 import {format} from "date-fns"
 
-const Header = () => {
+const Header = ({type}) => {
 
     const [openDate,setOpenDate] = useState(false);
     const [date, setDate] = useState([
@@ -35,7 +35,7 @@ const Header = () => {
 
     return (
         <div className='header'>
-            <div className='headerContainer'>
+            <div className={type === "list" ? "headerContainer listMode" : "headerContainer" }>
                 <div className='headerList'>
                     <div className='headerListItem active'>
                         <FontAwesomeIcon icon={faBed} />
@@ -58,9 +58,11 @@ const Header = () => {
                         <span>Airport Taxis</span>
                     </div>
                 </div>
+                {type !== "list" && <>
                 <h1 className='headerTitle'>A lifetime of discounts? It's Genius.</h1>
                 <p className='headerDesc'>Get rewarded for your travels - unlock instant savings of 10% or more with a free booking account</p>
                 <button className='headerBtn'>Sign in / Register</button>
+
                 <div className='headerSearch'>
                     <div className='headerSearchItem'>
                         <FontAwesomeIcon icon={faBed} className="headerIcon" />
@@ -112,6 +114,7 @@ const Header = () => {
                         <button className='headerBtn'>Search</button>
                     </div>
                 </div>
+                </>}
             </div>
         </div>
     )
--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Navbar,Header} from "../../components/"
 
 const List = () => {
     return (
